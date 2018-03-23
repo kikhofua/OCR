2f5bbe5710d8f0ef8f3963f58a0194352d18dd89
Kamoya Ikhofua <kki@mit.edu>
2018-03-23 16:53:34 -0400

    initial macro expander

---
+++ poggio/macro_expander.py
@@ -0,0 +1,20 @@
+import os
+
+
+def expand():
+    directory = "../data/faux_latex"
+    for filename in os.listdir(directory):
+        swap_file = os.path.join(directory, filename+"_swap")
+        with open(swap_file, "w+") as fswap:
+            for line in open(os.path.join(directory, filename)):
+                if "\\def" in line:
+                    line = line.replace("\\def", "\\newcommand")
+                    fswap.write(line)
+                else:
+                    fswap.write(line)
+
+    return
+
+
+if __name__ == "__main__":
+    expand()
